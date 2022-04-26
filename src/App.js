a1888fe2515eb9b2e5d13c27b1fa4dd08dc3b916
jj77847 <81172135+jj77847@users.noreply.github.com>
2022-04-26 12:53:28 +0100

    Updated TODOS

--- src/App.js
+++ src/App.js
@@ -4,16 +4,21 @@ import { useDispatch, useSelector } from "react-redux";
 import { useState } from "react";
 
 const Todos = () => {
+  const dispatch = useDispatch();
   const todos = useSelector((state) => state.todos);
+  const handleClick = (id) =>
+    dispatch({
+      type: "DELETE_TODO",
+      payload: id,
+    });
   if (!todos || !todos.length) {
     return <p>NO TODOS</p>;
   }
   return (
     <ul>
       {todos.map((todo) => (
-        <li>{todo.label}</li>
+        <li onClick={() => handleClick(todo.id)}>{todo.label}</li>
       ))}
-      )
     </ul>
   );
 };
